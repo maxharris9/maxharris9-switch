4ade71f9722f5ca522b934e40d69031d87593e54
Max Harris <mharris@leportschools.com>
2015-03-03 07:53:01 -0800

    package bump

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: "Switch component for Meteor",
-  version: "1.0.2",
+  version: "1.0.3",
   git: "https://github.com/max-leportlabs/maxharris9-switch.git",
   documentation: null
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0.3');
-  api.use(['reactive-var', 'templating', 'maxharris9:template-instance-utils@1.0.3', 'maxharris9:straightsix@1.0.1'], 'client');
+  api.use(['reactive-var', 'templating', 'maxharris9:template-instance-utils@1.0.3', 'maxharris9:straightsix@1.0.2'], 'client');
 
   api.addFiles('styles.js', 'client');
   api.addFiles('switch.html', 'client');
